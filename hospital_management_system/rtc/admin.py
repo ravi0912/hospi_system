59cfe3c25bfb4c0694d56dee1af52c2dc93447bc
Anipik <anirudhagnihotry098@gmail.com>
2015-04-04 00:03:23 +0530

    changed

--- hospital_management_system/rtc/admin.py
+++ hospital_management_system/rtc/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from rtc.models import CRequest,Medicine ,Salt
+from rtc.models import CRequest,Medicine,FollowUp , Complaint
 # Register your models here.
 admin.site.register(CRequest)
 admin.site.register(Medicine)
-admin.site.register(Salt)
+admin.site.register(FollowUp)
+admin.site.register(Complaint)
--- hospital_management_system/rtc/models.py
+++ hospital_management_system/rtc/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 # Anirudh Agnihotry
 status_choice=(('0','Waiting'),('1','Pending'),('2','Completed'))
+rating=(('1','poor'),('2','Average'),('3','good'),('4','very good'),('5','excellent'))
+category=(('1','poor'),('2','Average'),('3','good'),('4','very good'),('5','excellent'))
 # Create your models here.
 class CRequest(models.Model):
 	name = models.CharField(max_length = 140)
@@ -19,19 +21,41 @@ class CRequest(models.Model):
 	def __unicode__(self):
 		return self.name
 
-class Salt(models.Model):
-	name = models.CharField(max_length=140)
-
-	def __unicode__(self):
-		return self.name
 
 class Medicine(models.Model):
 	name = models.CharField(max_length=140)
-	salt = models.ManyToManyField('Salt')                                                                                                                                                             
+	salt = models.TextField(blank=True) 
+	mg = models.IntegerField()
+	price = models.FloatField()
+	disease = models.TextField(blank=True)
+	misc = models.TextField(blank=True)
+	prc = models.BooleanField()
+
 
 	def __unicode__(self):
 		return self.name
 
 
 
-#Anirudh Agnihotry
+class FollowUp(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+	rating = models.CharField(max_length=1,choices=rating)
+
+	def __str__(self):
+		return self.title
+
+class Complaint(models.Model):
+	username = models.CharField(max_length=140)
+	category = models.CharField(max_length=10,choices=category)
+	subject = models.CharField(max_length=140)
+	complaint = models.TextField(blank=True)
+	complaint_date = models.DateTimeField()
+	doc_pat = models.BooleanField(default=0)
+
+	def __str__(self):
+		return self.subject
+
+
+
+
